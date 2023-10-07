2ade2b9d32256fb3006d6a5e6431eb2dfee0ce4a
vineet-vishwakarma <vineet22vishwakarma@gmail.com>
2023-10-08 01:11:41 +0530

    Downloading Functionality has been added

--- lib/fullscreen.dart
+++ lib/fullscreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_downloader/image_downloader.dart';
 
 class FullScreen extends StatefulWidget {
   final String imageurl;
@@ -22,38 +24,94 @@ class _FullScreenState extends State<FullScreen> {
     await WallpaperManager.setWallpaperFromFile(file.path, location);
   }
 
+  Future<void> downloadWallpaper() async {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text('Downloading Started')));
+
+    try {
+      // Saved with this method.
+      var imageId = await ImageDownloader.downloadImage(widget.imageurl);
+      if (imageId == null) {
+        return;
+      }
+
+      // Below is a method of obtaining saved image information.
+      var fileName = await ImageDownloader.findName(imageId);
+      var path = await ImageDownloader.findPath(imageId);
+      var size = await ImageDownloader.findByteSize(imageId);
+      var mimeType = await ImageDownloader.findMimeType(imageId);
+    } on PlatformException catch (error) {
+      print(error);
+    }
+
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text('Downloading Finished')));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                child: Image.network(widget.imageurl),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Image.network(
+                          widget.imageurl,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FloatingActionButton(
+                          onPressed: () => downloadWallpaper(),
+                          elevation: 10,
+                          backgroundColor: Colors.black,
+                          tooltip: 'Download Image',
+                          child: const Icon(
+                            Icons.download,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            InkWell(
-              onTap: () => setWallpaper(),
-              child: Container(
-                height: 60,
-                width: double.infinity,
-                color: Colors.black,
-                child: Center(
-                  child: Text(
-                    'Set Wallpaper',
-                    style: GoogleFonts.lato(
-                      textStyle: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
+              SizedBox(
+                height: 55,
+              ),
+              InkWell(
+                onTap: () => setWallpaper(),
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.black,
+                  child: Center(
+                    child: Text(
+                      'Set Wallpaper',
+                      style: GoogleFonts.lato(
+                        textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
