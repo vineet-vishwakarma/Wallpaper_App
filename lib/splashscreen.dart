5c68eab8158acdbc2d3373fa1f096be9ec52b738
vineet-vishwakarma <vineet22vishwakarma@gmail.com>
2023-10-09 02:45:40 +0530

    Added Splash Screen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/wallpaper.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const Wallpaper()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset("assets/images/splashscreen.png"),
+    );
+  }
+}
